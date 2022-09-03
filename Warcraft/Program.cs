c76ba958058fa72aea00af7e83a5361e1718b0a1
Sergey Tyryshkin <golovinsergey17@gmail.com>
2022-09-03 09:40:09 +0300

    create units.

--- Warcraft/Program.cs
+++ Warcraft/Program.cs
@@ -6,11 +6,11 @@ namespace Warcraft
     {
         static void Main()
         {
-            {
-                var dragon = new Dragon(10, 2.2, "dragon", 2.4, 50, 7, 8, 7, 80);
-                Console.WriteLine("hi.");
-                dragon.FireBreath();
-            }
+            var peasant = new Peasant(100, 1.5, "peasant", 2.4);
+            var footman = new Footman(150, 2.2, "footman", 2.2, 30, 1.1, 50);
+            var mage = new Mage(50, 15, "mage", 2, 50, 2, 10, 7, 80);
+            var guardTower = new GuardTower(450, 50, "guardTower", 5, 50, 1);
+            var dragon = new Dragon(1000, 150, "dragon", 5.5, 100, 3, 0, 8, 500);
         }
     }
 }
